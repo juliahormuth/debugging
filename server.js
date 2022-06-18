06450f152eb44c9ff69aca630a921461d03a3030
Julia Hormuth <jvhormuth@gmail.com>
2022-06-18 15:03:51 -0300

    debug in the terminal

--- server.js
+++
@@ -1,6 +0,0 @@
-const Http = require("http");
-const PORT = 3000;
-
-Http.createServer((req, res) => {
-  res.end("Hey there!");
-}).listen(PORT, () => console.log("The server is running at", PORT));
